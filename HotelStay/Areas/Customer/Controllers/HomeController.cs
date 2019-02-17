9769aef50f81c96dc65218b111ec6458e1b29f9e
Vincent Grice <39641488+vgrice888@users.noreply.github.com>
2019-02-17 13:46:07 -0500

    Create home page with list of rooms and create session for adding rooms to cart and removing rooms

--- HotelStay/Areas/Customer/Controllers/HomeController.cs
+++ HotelStay/Areas/Customer/Controllers/HomeController.cs
@@ -5,35 +5,67 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HotelStay.Models;
+using HotelStay.Data;
+using Microsoft.EntityFrameworkCore;
+using HotelStay.Extensions;
 
 namespace HotelStay.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Index()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            var roomList = await _db.Rooms.Include(m => m.RoomTypes).Include(m => m.RoomTags).ToListAsync();
+            return View(roomList);
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Details(int id)
         {
-            ViewData["Message"] = "Your contact page.";
+            var room = await _db.Rooms.Include(m => m.RoomTypes).Include(m => m.RoomTags).Where(m=>m.Id == id).FirstOrDefaultAsync();
 
-            return View();
+            return View(room);
         }
 
-        public IActionResult Privacy()
+        [HttpPost, ActionName("Details")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DetailsPost(int id)
         {
-            return View();
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int> > ("ssShoppingCart");
+            if(lstShoppingCart == null)
+            {
+                lstShoppingCart = new List<int>();
+            }
+            lstShoppingCart.Add(id);
+            HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            return RedirectToAction("Index", "Home", new { area = "Customer" });
         }
+
+        public IActionResult Remove(int id)
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if (lstShoppingCart.Count > 0)
+            {
+                if (lstShoppingCart.Contains(id))
+                {
+                    lstShoppingCart.Remove(id);
+                }
+            }
+
+            HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
