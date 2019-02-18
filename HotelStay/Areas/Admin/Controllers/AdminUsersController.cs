6f12f828a0cc48eeb70e7b5094ce4e18afdcb011
Vincent Grice <39641488+vgrice888@users.noreply.github.com>
2019-02-18 17:29:53 -0500

    Delete get action and post action methods created for admin users.

--- HotelStay/Areas/Admin/Controllers/AdminUsersController.cs
+++ HotelStay/Areas/Admin/Controllers/AdminUsersController.cs
@@ -34,8 +34,10 @@ namespace HotelStay.Areas.Admin.Controllers
             var userFromDb = await _db.ApplicationUser.FindAsync(id);
             if (userFromDb == null)
             {
-                return View(userFromDb);
+                return NotFound();
             }
+
+            return View(userFromDb);
            
         }
 
@@ -60,6 +62,35 @@ namespace HotelStay.Areas.Admin.Controllers
             }
 
             return View(applicationUser);
+        }
+
+        //GET : Delete
+        public async Task<IActionResult>Delete(string id)
+        {
+            if(id == null || id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+
+            var userFromDb = await _db.ApplicationUser.FindAsync(id);
+            if(userFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFromDb);
+        }
+
+        //POST : Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id, ApplicationUser applicationUser)
+        {
+            ApplicationUser userFromDb = _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefault();
+            userFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
